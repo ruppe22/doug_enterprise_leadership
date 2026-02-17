944c91ba0f245f9334e0f44fd6869326c1e20fff
ruppe22 <50990351+ruppe22@users.noreply.github.com>
2026-02-16 18:14:21 -0600

    Delete node_modules directory

--- node_modules/@keystatic/next/dist/declarations/src/api.d.ts
+++
@@ -1,3 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { APIRouteConfig } from '@keystatic/core/api/generic';
-export declare function makeAPIRouteHandler(_config: APIRouteConfig): (req: NextApiRequest, res: NextApiResponse) => Promise<Response | undefined>;
--- node_modules/@keystatic/next/dist/declarations/src/route-handler.d.ts
+++
@@ -1,5 +0,0 @@
-import { APIRouteConfig } from '@keystatic/core/api/generic';
-export declare function makeRouteHandler(_config: APIRouteConfig): {
-    GET: (request: Request) => Promise<Response>;
-    POST: (request: Request) => Promise<Response>;
-};
